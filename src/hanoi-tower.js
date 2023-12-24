b1a995342f6da52f68ab5110d1da51f354ce6442
kirkor313 <kirkor313@mail.ru>
2023-12-24 14:11:41 +0300

    feat: add solution of tasks

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,15 +14,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
-
-module.exports = {
-  calculateHanoi
-};
-
 
 function calculateHanoi(disksNumber, turnsSpeed) {
   const result = {};
@@ -30,3 +21,9 @@ function calculateHanoi(disksNumber, turnsSpeed) {
   result.seconds = Math.floor((3600 / turnsSpeed) * result.turns);
   return result
 }
+
+module.exports = {
+  calculateHanoi
+};
+
+
--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let res = true;
+  if (n.split("-").length !== 6) res = false;
+  n.split("-")
+    .map(el =>
+      !(/^[\da-f]/i).test(el)
+      ? res = false
+      : res
+    );
+  return res;
 }
 module.exports = {
   isMAC48Address
